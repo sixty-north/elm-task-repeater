f677c8a1d98c8ad43f2f0a0e41c5923b2150b72b
Austin Bingham <austin@sixty-north.com>
2016-10-20 08:57:58 +0200

    Abstracted poller for any task.

--- src/Poll.elm
+++ src/Poll.elm
@@ -1,16 +1,10 @@
 module Poll exposing (..)
 
 import Cmd.Extra exposing (message)
-import Http
-import Json.Decode
 import Platform.Cmd
 import Task
 import Task.Extra exposing (delay)
 import Time
-
-
-type alias URL =
-    String
 
 
 type Msg extmsg result
@@ -18,33 +12,29 @@ type Msg extmsg result
     | Multi (List extmsg)
 
 
-type alias Model extmsg result =
-    { url : URL
+type alias Model extmsg error result =
+    { task : Task.Task error result
     , period : Time.Time
-    , on_response : result -> extmsg
-    , on_error : Http.Error -> extmsg
+    , on_success : result -> extmsg
+    , on_error : error -> extmsg
     , msg_wrapper : Msg extmsg result -> extmsg
-    , result_decoder : Json.Decode.Decoder result
     , continue : result -> Bool
     }
 
 
-update : Msg extmsg result -> Model extmsg result -> ( Model extmsg result, Platform.Cmd.Cmd extmsg )
+update : Msg extmsg result -> Model extmsg error result -> ( Model extmsg error result, Platform.Cmd.Cmd extmsg )
 update msg model =
     case msg of
         Poll ->
             let
-                get =
-                    Http.get model.result_decoder model.url
-
                 task =
-                    delay model.period get
+                    delay model.period model.task
 
                 msgs result =
                     if (model.continue result) then
-                        [ model.on_response result, model.msg_wrapper Poll ]
+                        [ model.on_success result, model.msg_wrapper Poll ]
                     else
-                        [ model.on_response result ]
+                        [ model.on_success result ]
 
                 cmd =
                     Task.perform
@@ -58,6 +48,6 @@ update msg model =
             model ! List.map message msgs
 
 
-start : Model extmsg result -> Platform.Cmd.Cmd extmsg
+start : Model extmsg error result -> Platform.Cmd.Cmd extmsg
 start model =
     message (model.msg_wrapper Poll)
