660b649a0b1d0546594edbd12a5bb4be2ffea3c8
Austin Bingham <austin@sixty-north.com>
2016-10-24 16:26:31 +0200

    Documented the Schedulers module.

--- src/TaskRepeater/Schedulers.elm
+++ src/TaskRepeater/Schedulers.elm
@@ -4,18 +4,22 @@ module TaskRepeater.Schedulers exposing (exponentialBackoff, uniform)
 
 # Schedulers
 @docs uniform, exponentialBackoff
-
 -}
 
 import TaskRepeater exposing (scheduler)
 import Time
 
-{- A scheduler that puts a uniform time between each performance of the task. -}
 
+{-| Create scheduler that puts a uniform time between each performance of the
+task.
 
+To create a schedule that runs a task every 10 seconds:
+
+  uniform (Time.seconds * 10)
+-}
 uniform : Time.Time -> Scheduler Time.Time
 uniform period =
-    scheduler period (\last -> (last, last))
+    scheduler period (\last -> ( last, last ))
 
 
 type alias ExponentialParams =
@@ -25,6 +29,14 @@ type alias ExponentialParams =
     }
 
 
+{-| Create a scheduler that puts exponentially increasing time between each
+execution of the task.
+
+To create a scheduler that starts with a 2 second wait, doubles the wait time
+for each subsequent execution, and maxes out at 60 seconds:
+
+    exponentialBackoff (Time.second 2) 2 (Time.second 60)
+-}
 exponentialBackoff : Time.Time -> Float -> Time.Time -> Scheduler ExponentialParams
 exponentialBackoff period factor maxPeriod =
     let
