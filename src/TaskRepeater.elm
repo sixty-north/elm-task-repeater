e490bb6f778e79d19f7461bbdb95081c015e3699
Austin Bingham <austin@sixty-north.com>
2016-10-24 09:35:05 +0200

    Improved documentation.

--- src/TaskRepeater.elm
+++ src/TaskRepeater.elm
@@ -1,4 +1,27 @@
-module TaskRepeater exposing (Model, Msg, Scheduler, start, update)
+module TaskRepeater exposing (model, Model, Msg, scheduler, start, update)
+
+{-| Provides a repeatedly running a Task and communicating the results (success
+or error) to other parts of a program.
+
+You provide a few pieces of information:
+ - the task to run
+ - a scheduler for determining when to run the task
+ - messages for communicating success and error
+ - a message for wrapping internal messages that should come back to the task
+   repeater framework
+ - a function determining when to continue running the task
+
+Based on these TaskRepeater will repeated execute the task, sending back the
+results via the messages you configure it with.
+
+# Factories
+@docs model, scheduler
+
+# Execution
+@docs start, update
+
+TODO: Example
+-}
 
 import Cmd.Extra exposing (message)
 import Platform.Cmd
@@ -7,6 +30,8 @@ import Task.Extra exposing (delay)
 import Time
 
 
+{-| Messages used for internal operations.
+-}
 type Msg extmsg
     = Poll
     | Multi (List extmsg)
@@ -17,51 +42,91 @@ type alias Scheduler m =
     , next : m -> ( m, Time.Time )
     }
 
-type alias Model extmsg error result s =
+
+{-| Create a schedule for task execution.
+
+`model` is whatever data the scheduler needs to do its job.
+
+`next` determines how long to wait before the next task execution. Given the
+current model, it produces the next model and the delay time.
+-}
+scheduler : m -> (m -> ( m, Time.Time )) -> Scheduler m
+scheduler =
+    Scheduler
+
+
+type alias ModelRecord error result s extmsg =
     { task : Task.Task error result
     , scheduler : Scheduler s
-    , on_success : result -> extmsg
-    , on_error : error -> extmsg
-    , msg_wrapper : Msg extmsg -> extmsg
+    , onSuccess : result -> extmsg
+    , onError : error -> extmsg
+    , msgWrapper : Msg extmsg -> extmsg
     , continue : result -> Bool
     }
 
 
-update : Msg extmsg -> Model extmsg error result scheduler -> ( Model extmsg error result scheduler, Platform.Cmd.Cmd extmsg )
+type Model error result s extmsg
+    = Model (ModelRecord error result s extmsg)
+
+
+{-| Create a new model.
+
+`task` is the task to be periodically executed.
+`scheduler` determines when the task will be executed.
+`onResult` creates the message that will be issued when the task succeeds.
+`onError` creates the message that will be issued when the task fails.
+`msgWrapper` creates a message that wraps a Msg so that users can route messages to `update`.
+`continue` determines if execution should continue when a result is received.
+-}
+model : Task.Task e r -> Scheduler s -> (r -> m) -> (e -> m) -> (Msg m -> m) -> (r -> Bool) -> Model e r s m
+model task scheduler onResult onError msgWrapper continue =
+    Model (ModelRecord task scheduler onResult onError msgWrapper continue)
+
+
+{-| Process a Msg to update a Model.
+
+A standard update function.
+-}
+update : Msg m -> Model e r s m -> ( Model e r s m, Platform.Cmd.Cmd m )
 update msg model =
-    case msg of
-        Poll ->
-            let
-                ( scheduler_model, period ) =
-                    model.scheduler.next model.scheduler.model
+    case model of
+        Model rec ->
+            case msg of
+                Poll ->
+                    let
+                        ( schedulerModel, period ) =
+                            rec.scheduler.next rec.scheduler.model
 
-                s =
-                    model.scheduler
+                        scheduler =
+                            Scheduler schedulerModel rec.scheduler.next
 
-                scheduler =
-                    { s | model = scheduler_model }
+                        task =
+                            delay period rec.task
 
-                task =
-                    delay period model.task
+                        msgs result =
+                            if (rec.continue result) then
+                                [ rec.onSuccess result, rec.msgWrapper Poll ]
+                            else
+                                [ rec.onSuccess result ]
 
-                msgs result =
-                    if (model.continue result) then
-                        [ model.on_success result, model.msg_wrapper Poll ]
-                    else
-                        [ model.on_success result ]
+                        cmd =
+                            Task.perform
+                                rec.onError
+                                (msgs >> Multi >> rec.msgWrapper)
+                                task
+                    in
+                        Model { rec | scheduler = scheduler } ! [ cmd ]
 
-                cmd =
-                    Task.perform
-                        model.on_error
-                        (msgs >> Multi >> model.msg_wrapper)
-                        task
-            in
-                { model | scheduler = scheduler } ! [ cmd ]
-
-        Multi msgs ->
-            model ! List.map message msgs
+                Multi msgs ->
+                    model ! List.map message msgs
 
 
-start : Model extmsg error result scheduler -> Platform.Cmd.Cmd extmsg
+{-| Start executing a task on a schedule.
+
+This will return a Cmd that should be routed normally by the caller.
+-}
+start : Model e r s m -> Platform.Cmd.Cmd m
 start model =
-    message (model.msg_wrapper Poll)
+    case model of
+        Model rec ->
+            message (rec.msgWrapper Poll)
